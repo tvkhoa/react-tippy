74746a0151872c626c928a285b4f318b41aa4a0d
scf4 <scf4@github.com>
2017-07-21 19:47:29 +0100

    Added SSR check

--- src/Tooltip/js/utils/matches.js
+++ src/Tooltip/js/utils/matches.js
@@ -1,12 +1,15 @@
+function defaultMatchSelector(s) {
+  var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+      i = matches.length;
+  while (--i >= 0 && matches.item(i) !== this) {}
+  return i > -1;
+}
+
 export const matches =
+  typeof window === 'undefined' ? defaultMatchSelector :
   Element.prototype.matches               ||
   Element.prototype.matchesSelector       ||
   Element.prototype.webkitMatchesSelector ||
   Element.prototype.mozMatchesSelector    ||
   Element.prototype.msMatchesSelector     ||
-  function(s) {
-      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-          i = matches.length;
-      while (--i >= 0 && matches.item(i) !== this) {}
-      return i > -1;
-  }
+  defaultMatchSelector
