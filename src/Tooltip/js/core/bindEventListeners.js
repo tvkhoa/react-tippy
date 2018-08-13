52cc6a54c70d6e483cb6ee8dde002d10fd7c9fbe
Phạm Tuấn <phamtuan2993@gmail.com>
2018-08-13 10:56:40 +0700

    Fix settings of undefined

--- src/Tooltip/js/core/bindEventListeners.js
+++ src/Tooltip/js/core/bindEventListeners.js
@@ -52,12 +52,16 @@ export default function bindEventListeners() {
 
     if (popper) {
       const ref = find(Store, ref => ref.popper === popper)
+      if (!ref) return;
+
       const { settings: { interactive } } = ref
       if (interactive) return
     }
 
     if (el) {
       const ref = find(Store, ref => ref.el === el)
+      if (!ref) return;
+
       const {
         settings: {
           hideOnClick,
